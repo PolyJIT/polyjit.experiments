977c77ca1c651520e196d6be8f05f22fbb1b6596
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2018-07-10 16:37:40 +0200

    init: import all experiments by default when using * import

---
+++ polyjit/experiments/__init__.py
@@ -0,0 +1,25 @@
+"""
+Import all experiments by default.
+"""
+# yapf: disable
+__all__ = [
+    "compilestats",
+    "papi",
+    "ijpp",
+    "mse",
+    "pgo",
+    "pj_collect",
+    "pj_cs",
+    "pj_likwid",
+    "pj_papi",
+    "pj_perf",
+    "pj_raw",
+    "pj_sequence",
+    "pjtest",
+    "polly",
+    "pollytest",
+    "polyjit",
+    "profileScopDetection",
+    "sequences"
+]
+# yapf: enable
