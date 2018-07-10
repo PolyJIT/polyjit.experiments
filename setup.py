2f9319c169109d827278c2d6ac6a41133d130274
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2018-07-10 12:47:08 +0200

    initial project structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='benchbuild',
+    use_scm_version=True,
+    url='https://github.com/PolyJIT/polyjit.experiments',
+    packages=find_packages(),
+    setup_requires=["setuptools_scm"],
+    tests_require=["pytest"],
+    install_requires=["benchbuild==3.2.0"],
+    author="Andreas Simbuerger",
+    author_email="simbuerg@fim.uni-passau.de",
+    description="Additional experiments used by PolyJIT with BenchBuild.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license="MIT",
+    classifiers=[
+        'Development Status :: 4 - Beta', 'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords="benchbuild experiments")
